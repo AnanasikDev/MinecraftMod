9b20852157dd7a40c30f64c1821949bedd27e03e
oleg <oleg.brovchenkov@gmail.com>
2024-04-06 11:59:50 +0200

    Init. Custom item & Creative Mode Tab

---
+++ src/main/java/net/ananaseek/tutorialmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,31 @@
+package net.ananaseek.tutorialmod.item;
+
+import net.ananaseek.tutorialmod.TutorialMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MOD_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MOD_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.SAPPHIRE.get());
+                        output.accept(Items.EMERALD);
+                        output.accept(Items.DIAMOND);
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus){
+        CREATIVE_MOD_TABS.register(eventBus);
+    }
+}
